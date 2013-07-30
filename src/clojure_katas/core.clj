233dcc663254bed98684d5c2fed85cf79e06e7ae
Marshall Shen <mshen@groupon.com>
2013-07-30 15:55:38 -0500

    work step by step, got the first scenario pass

--- src/clojure_katas/core.clj
+++ src/clojure_katas/core.clj
@@ -5,3 +5,17 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def numerals
+  {\I 1
+   \V 5
+   \X 10
+   \L 50
+   \C 100
+   \D 500
+   \M 1000})
+
+
+(defn roman->arabic
+  [s]
+  (get numerals s))
