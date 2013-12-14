57fd842ac3c3bcf1b838cfb259956419f9df0c76
Marshall Shen <mshen@groupon.com>
2013-12-14 14:26:24 -0600

    Fibonacci seq: Add iterative recursion. - Perform linear bound variable transformation is much more efficient   than tree like recursion.

--- test/clojure_katas/nth_fibonacci_test.clj
+++ test/clojure_katas/nth_fibonacci_test.clj
@@ -2,12 +2,22 @@
   (:use clojure.test
         clojure-katas.nth-fibonacci))
 
-(deftest nth-fibonacci-test
-  (testing "return nth fibonacci number"
-    (is (= 0 (nth-fib -100)))
-    (is (= 1 (nth-fib 0)))
-    (is (= 1 (nth-fib 1)))
-    (is (= 2 (nth-fib 2)))
-    (is (= 3 (nth-fib 3)))
-    (is (= 5 (nth-fib 4)))
-    (is (= 8 (nth-fib 5)))))
+(deftest nth-fibonacci-tree-test
+  (testing "return nth fibonacci number, using tree recursion"
+    (is (= 0 (nth-fib-tree -100)))
+    (is (= 1 (nth-fib-tree 0)))
+    (is (= 1 (nth-fib-tree 1)))
+    (is (= 2 (nth-fib-tree 2)))
+    (is (= 3 (nth-fib-tree 3)))
+    (is (= 5 (nth-fib-tree 4)))
+    (is (= 8 (nth-fib-tree 5)))))
+
+(deftest nth-fibonacci-iter-test
+  (testing "return nth fibonacci number, using iterative recursion"
+    (is (= 0 (nth-fib-iter -100)))
+    (is (= 1 (nth-fib-iter 0)))
+    (is (= 1 (nth-fib-iter 1)))
+    (is (= 2 (nth-fib-iter 2)))
+    (is (= 3 (nth-fib-iter 3)))
+    (is (= 5 (nth-fib-iter 4)))
+    (is (= 8 (nth-fib-iter 5)))))
